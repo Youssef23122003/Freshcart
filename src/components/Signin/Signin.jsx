780d39776fa1f7df81d929654b14c34351914e98
besheir77 <bassammohamed398@gmail.com>
2025-03-24 21:09:56 +0200

    first commit

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -70,7 +70,7 @@ let formik= useFormik({
   return <>
   <h2 className='text-4xl text-green-600 font-bold  my-4'>Register Now</h2>
 
-<form onSubmit={formik.handleSubmit} className="max-w-md mx-auto">
+<form onSubmit={formik.handleSubmit} className="">
 
   <div className="relative z-0 w-full my-5 group">
       <input value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
@@ -94,7 +94,7 @@ let formik= useFormik({
  {apiError}
 </div>:null}
   <Link className='text-green-500' to={'/ForgetPassword'}>Forgot Password</Link>
-  <button type="submit" class="focus:outline-none block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> {loading ?  <i className='fa-solid fa-spinner fa-spin'></i>:'Submit'}   </button>
+  <button type="submit" class="focus:outline-none block mt-2 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> {loading ?  <i className='fa-solid fa-spinner fa-spin'></i>:'Submit'}   </button>
 
   </form>
   
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -90,7 +90,7 @@ let formik= useFormik({
   
   <h2 className='text-4xl text-green-600 font-bold  my-4'>Register Now</h2>
 
-<form onSubmit={formik.handleSubmit} className="max-w-md mx-auto">
+<form onSubmit={formik.handleSubmit} className="">
   <div className="relative z-0 w-full my-5 group">
       <input value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} type="text" name="name" id="userName" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "   />
       <label htmlFor="userName" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter your Name</label>
@@ -144,7 +144,7 @@ let formik= useFormik({
  {apiError}
 </div>:null}
 
-  <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> {loading ?  <i className='fa-solid fa-spinner fa-spin'></i>:'Submit'}   </button>
+  <button type="submit" class="focus:outline-none text-center text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> {loading ?  <i className='fa-solid fa-spinner fa-spin'></i>:'Submit'}   </button>
 
   </form>
   
